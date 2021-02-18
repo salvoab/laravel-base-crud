ac5283475dbc84216af6f76fc2d2764ae2ddd12d
salvoab <salvatore.abate85@gmail.com>
2021-02-18 16:50:12 +0100

    Eliminato container duplicato in posts/index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,20 +10,21 @@
         <p class="lead">Un elenco di tutti i post scritti in questo blog</p>
     </div>
     
-    <div class="container">
-        <div class="row justify-content-start">
+
+    <div class="row justify-content-start">
         @foreach($all_posts as $post)
             <div class="col-4">
-      
+        
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">{{ $post->title }}</h4>
                         <p class="card-text">{{ $post->body }}</p>
                     </div>
                 </div>
-            
+                
             </div>
         @endforeach    
     </div>
+    
 
 @endsection
