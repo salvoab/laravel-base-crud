101b07bbe0dbe3e8d3e1b045d3679a962d696bd3
salvoab <salvatore.abate85@gmail.com>
2021-02-18 15:48:45 +0100

    Creato un layout di base da estendere
    Creato layout e rinominata pagina welcome in home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    <h1>Blog Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laborum adipisci, sint voluptas fuga nam porro recusandae. Placeat et assumenda numquam neque, non omnis cum dicta itaque veritatis dignissimos commodi?</p>
+@endsection
